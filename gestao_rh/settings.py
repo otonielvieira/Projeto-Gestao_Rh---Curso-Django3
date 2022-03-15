42476093ec8dc272ec4c555410af5fe24d08d27b
otonielvieira <ottobuzios@gmail.com>
2022-03-15 20:56:25 -0300

    Criando app Funcionarios e corrigindo secretkey

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mcc%vt_nz4a0(_(b=eivww8qmvhgfferyizyv@ay&clhryi3nc'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apps.empresas'
+    'apps.empresas',
+    'apps.funcionarios',
 ]
 
 MIDDLEWARE = [
